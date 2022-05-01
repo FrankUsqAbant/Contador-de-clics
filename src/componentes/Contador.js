1ce3b400cd45df88fc8d2671b4e76faf4015be03
Frank <franabanto12@gmail.com>
2022-05-01 16:26:40 -0500

    Contador de clics

---
+++ src/componentes/Contador.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../hojas-de-estilo/Contador.css";
+
+function Contador({ numClics }) {
+  return <div className="contador">{numClics}</div>;
+}
+export default Contador;
